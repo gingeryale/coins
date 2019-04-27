6b164ea0ce9e6b59fc78161fafb39bcf7b512fb3
ao <aolin@ymail.com>
2019-04-27 10:38:35 +0300

    deleted commented out js

--- js/templateU.js
+++ js/templateU.js
@@ -1,22 +1,8 @@
 // $.getScript('js/templateF.js');
 
 
-// $(".checkboxer").on('click', function(){
-//  var maxAllowed = 2;
-//  var count = $("checkboxer + .active").length;
-//   if(count > maxAllowed){
-//     // $(this).prop("checked", "");
-//       alert("reached max "+ maxAllowed);
-//     }
-//});
-
-
-
 var boxer = "input[name='coins']:checked";
 
-// $("input:checkbox[name=type]:checked").each(function(){
-//   selection.push($(this).val());
-// });
 function addCoin(){
     // start with empty array
     var selection = [];
@@ -33,21 +19,10 @@ function addCoin(){
     // $('#show').html(selection);
     
 }
-// /*binds run function to click of submit*/
-// $('#submit').click(run);
-// console.log(selection);
+
 function arrayRunner(){
   $("#arrayResults").empty();
   $('#exampleModalCenter').modal('show');
-  // for(let i = 0; i < selection.length; i++){
-  //     let arr = `<li>${selection[i]}<button class="del">x</button></li>`
-  //     $("#arrayResults").append(arr);
-  // }
-
-  // $('#table').find(boxer).each(function (el) {
-  //   let arr = `<li>${el.value}<button class="del">x</button></li>`
-  //     $("#arrayResults").append(arr);
-  // });
 
   $("input[name='coins']:checked").each(function () {
     //console.log($(this).val());
@@ -55,11 +30,7 @@ function arrayRunner(){
        $("#arrayResults").append(arr);
        console.log(arr);
 });
-// delete from checked selection
-  // $('.del').click(function(){
-  //   console.log(this);
-  //   console.log($(this));
-  // });
+
   deleteOnefromModal();
 }
 
@@ -81,23 +52,6 @@ function removeFromSelectionArray(userSelectedCoin){
  
 }
 
-
-
-          //   function buildArr(id) {
-          //     let allBoxes = document.getElementsByClassName('input[type="checkbox"]');
-          //     console.log("here are all " +allBoxes);
-          //     for (var i = 0; i < allBoxes.length; i++) {
-          //         if (allBoxes[i].checked) {
-          //             sessionStorage.setItem(id + i, allBoxes[i].value);
-          //             console.log("all of them " +allBoxes[i].checked);
-          //         }
-          //     }
-  
-          //     //location = "c.html";
-             
-          // }
-          
-          
 
 
 
