5282299dd7cc9e05060af9538e216055e88cd8f8
ao <aolin@ymail.com>
2019-05-03 11:17:17 +0300

    not there

--- js/templateU.js
+++ js/templateU.js
@@ -24,11 +24,11 @@ function addCoin(){
     $('.userSelection_array').text(selection);
 }
 
-// display all checkboxes selected in modal
+// Modal display
 function arrayRunner(){
   $("#arrayResults").empty();
   $('#exampleModalCenter').modal('show');
-
+// Display coins of checkboxes on page
   $("input[name='coins']:checked").each(function () {
     //console.log($(this).val());
     let arr = `<li><span class="selectListDel">${$(this).val()}</span><button data-coin="${$(this).val()}" class="del btn btn-secondary">Remove</button></li>`
@@ -41,10 +41,36 @@ function arrayRunner(){
 function deleteOnefromModal(selection){
   $(".del").attr("data-name","coin").click(function(){
     let userSelectedCoin = $(this).data('coin');
+
+    console.log("user removed the following coin:");
     console.log(userSelectedCoin);
-   
+   // remove from Modal UI
     $(this).closest('li').remove();
     removeFromSelection(userSelectedCoin, selection);
+
+    // set checkbox to unchecked 
+    //document.getElementById(`[data-target='${userSelectedCoin}']`).checked = false;
+    $(`[data-target='${userSelectedCoin}']`).prop("checked", false);
+
+    // remove from selection array
+
+    // add all existing values to the selection array
+    let articles = document.querySelectorAll('.del');
+    const nodelistToArray = Array.apply(null, articles);
+    nodelistToArray.forEach(delBtns => {
+      selection.push(delBtns.dataset.coin);
+    });
+    
+    // reduce Array
+    // var rawArr = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd'];
+    var reducedArray = selection.reduce(function (accumulator, currentValue) {
+      if (accumulator.indexOf(currentValue) === -1) {
+        accumulator.push(currentValue);
+      }
+      return accumulator
+    }, []);
+    console.log("here is CLEAN ARRAY");
+    console.log(reducedArray);
   });
 }
 
@@ -58,22 +84,27 @@ function removeFromSelection(userSelectedCoin, selection){
   console.log(selection);
   // is found to exist
     //if($.inArray(userSelectedCoin, selection) !== -1){
-      if(selection.includes(userSelectedCoin.toString())){
-      selection.splice(selection.indexOf(userSelectedCoin), 1);
-      console.log(selection);
+
+
+
+      // if(selection.includes(userSelectedCoin.toString())){
+      // selection.splice(selection.indexOf(userSelectedCoin), 1);
+      // console.log(selection);
+
+
 
       //  rebuild the actual array 
-      let articles = document.querySelectorAll('.del');
+      
       //  turn nodeList into Array
-      const nodelistToArray = Array.apply(null, articles);
+      
           //$(".del").each(function(){
             //LOOP OVER REMIANING DELETE BTNS THE USER DIDN'T DELETE AND WANTS TO KEEP//
             // REBUILD ARRAY FROM START WITH EMPTY ARRAY
-            selection=[];
-            nodelistToArray.forEach(delBtns => {
-              selection.push(delBtns.dataset.coin);
-            });
+           
+
+            // UI indication of removed element
             let userDelCoin = document.querySelector(`[data-target='${userSelectedCoin}']`);
+            // UI if user adds back to list
               userDelCoin.parentNode.classList.add('goneGirl');
               $('.goneGirl').click(function(){
                 $(this).removeClass('goneGirl');
@@ -85,11 +116,17 @@ function removeFromSelection(userSelectedCoin, selection){
       //});
       console.log("again");
       console.log(selection);
+
+
+      
+
+
+
       sessionStorage.setItem('selection', selection);
-      // hit the save button and have appear
+      // take whatever's left in modal
       $('.userSelection_array').text(selection);
     } 
-}
+
 
 
 // disable checkboxes that go beyond 5 selections
