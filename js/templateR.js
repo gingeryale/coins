33880a66be474ce5cd3396191dc44201127ca4b2
gingery ale <aolin@ymail.com>
2019-05-11 18:58:18 +0300

    charts still don't update or render lines

--- js/templateR.js
+++ js/templateR.js
@@ -3,32 +3,28 @@ var time = new Date();
 time.setMinutes();
 var dataPoints = [];
 
-var selectionReport = $('.userSelection_array').toArray().map(elem => elem.innerHTML); 
-  if(searchCondition.length > 0){
-  console.log("more than 1");
-  }else{
-    $("#searchResult").empty().html('<h3>Live Reports needs a minimum of one and maximum of 5 coins to run.</h3>');
-  };
+  
+//var storedNames = JSON.parse(JSON.stringify(sessionStorage.getItem("selection")));
+// console.log("storedNamesstoredNamesstoredNames is here:");
+// console.log(storedNames);
+var storedNames = "BTC,808,777,888,ETH";
 
-var storedNames = JSON.parse(JSON.stringify(sessionStorage.getItem("selection")));
-console.log("storedNamesstoredNamesstoredNames is here:");
-
-console.log(storedNames);
-console.log("selectionReport is here:");
-console.log(selectionReport);
-
+if(storedNames.length > 0){
+	console.log("more than 1");
+	}else{
+	  $("#searchResult").empty().html('<h3>Live Reports needs a minimum of one and maximum of 5 coins to run.</h3>');
+};
 
 var urls = "https://min-api.cryptocompare.com/data/pricemulti?fsyms="+storedNames+"&tsyms=USD";
-//var urls = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,888,ETH&tsyms=USD";
+//var urls = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,808,777,888,ETH&tsyms=USD";
 console.log(urls);
+
 var $promise = $.ajax({
 	url: urls,
 	dataType: 'json',
 	type: 'GET',
 	data: data,
 	success: function (response, status) {
-		//var arr = JSON.parse(JSON.stringify(response));
-		//debugger;
 		var arr = JSON.stringify(response);
 		console.log(arr);
 		if(response.Response !== 'Error'){
@@ -70,8 +66,6 @@ console.log(dataPoints);
 			},
 			data: dataPoints 
 		});
-		//console.log(chart.options.data[0]);
-		//console.log(chart.options.data[0].dataPoints);
 		chart.render();
 		
 		function toggleDataSeries(e){
@@ -90,18 +84,19 @@ function updateChart() {
 		//debugger;
 		var arra = JSON.parse(JSON.stringify(response));
 		console.log(arra);
+		//debugger;
+		//dataPoints[0].dataPoints[0].x
 			$.each(arra, function(i, e) {
-
 				dataPoints.push({
-					x: parseInt(i[0]),
-					y: parseInt(e[1])
+					x: parseInt(i),
+					y: parseInt(e)
 					});
 		 });
 		 chart.render();
 		 setTimeout(function(){updateChart()}, 2000);
 	});
 		} 
-		
+	// 200 OK response returns error for user selection
 } else{
 	$("#searchResult").empty().html('<h3>No coin data was returned from your selection. Select a new set of coins.</h3>');
 }
@@ -111,27 +106,4 @@ function updateChart() {
 			$("#table").empty().html('<h3>Error: A server error occurred.</h3>');
 	}
 });
-
-
-
-
-
-
-// var dataPoints = [];
-// $.getJSON("https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=USD", function(data) {  
-// 		$.each(data, function(key, value){
-// 				dataPoints.push(	{x: time.getMinutes(), y: `${value.USD}`} );
-// 		});
-// 		chart = new CanvasJS.Chart("chartContainer",{
-// 				title:{
-// 						text:"Live Chart with dataPoints from External JSON"
-// 				},
-// 				data: [{
-// 				type: "line",
-// 				dataPoints : dataPoints,
-// 				}]
-// 		});
-// 		chart.render();
-// });
-
 
